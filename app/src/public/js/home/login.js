4c9bafdf80aa09925e207fa12194eaaadeeb0792
Hwang Seok O <ohs6006@gmail.com>
2022-11-29 17:38:34 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log("Hello!");
+const id = document.querySelector("#id"),
+  password = document.querySelector("#password"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(e) {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "appliction/json",
+    },
+    body: JSON.stringify(req),
+  });
+}
