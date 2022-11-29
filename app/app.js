753f5d0e07be2e92c10d7befbc05516c9bbc1206
Hwang Seok O <ohs6006@gmail.com>
2022-11-29 15:58:49 +0900

    프론트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// JS연결하기
+app.use(express.static(`${__dirname}/src/public`));
+
 // 라우터 불러오기
 app.use("/", home); // use => 미들 웨어를 등록해주는 메서드
 
